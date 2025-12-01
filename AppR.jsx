95a4aa78d0e8e6c95aa18f0f90a64a2687360e89
clau9479 <sienna_24@hotmail.com>
2025-12-01 04:30:12 -0300

    estructura en html de mi proyecto

--- AppR.jsx
+++ AppR.jsx
@@ -4,17 +4,48 @@ import Notas from "./Notas.jsx";
 
 export default function AppR() {
   return (
-    <>
+    
+    <div className="contenedor">
     <h1>To Do App React</h1>
-    <div className="">
-      <Notas/>
-      <button className=""></button>
+      
+      <div className="Formulario">
+      <input
+        type="text"
+        placeholder="Nueva tarea"
+        value={textonuevo}
+        onchange={(e)=> setTextoNuevo(e.target.value)}
+        className="input" 
+        />
+      <button className="boton" onClick={agregarTarea}>Agregar</button>
+     </div>
+      
+      <input type="text"
+      placeholder="Buscar Tarea"
+      value={busqueda}
+      onChange={(e)=>setBusqueda(e.target.value)}
+      className="input"/>
+      
+      <button className="botonRojo"
+      onClick={eliminarCompletadas}>Elimninar completadas</button>
+
+      <div className="lista">
+        {tareasOrdenadas.map((t) =>(
+        <Notas
+        key={t.id}
+        tarea={t}
+        toggleCompletada={togglecompletada}
+        eliminarTarea={eliminarTarea}
+        cambiarColor={cambiarColor}
+        />
+      ))}
+      </div>
+
     </div>
     
     
     
-    </>
-  )
+  
+  );
 }
 
 
