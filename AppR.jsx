56e9beb74af016a3aeaffe02ed990e91347007d7
clau9479 <sienna_24@hotmail.com>
2025-12-01 07:37:01 -0300

    se agrego funciones a AppR.jsx

--- AppR.jsx
+++ AppR.jsx
@@ -1,19 +1,76 @@
-import React from "react";
+import { useState } from "react";
 import Notas from "./Notas.css";
 import Notas from "./Notas.jsx";
 
 export default function AppR() {
+  const[tareas,setTareas]= useState(()=>{
+    const guardadas=localStorage.getItem("tareas");
+    return guardadas ? JSON.parse(guardadas) :[];
+  });
+  const [textoNuevo,setTextoNuevo]= useState("");
+  const [busqueda,setBusqueda]= useState("");
+
+  const guardarLs =(nuevasTareas) =>{
+    localStorage.setItem("tareas", JSON.stringify(nuevasTareas));
+  };
+
+  const agregarTarea = ()=>{
+    if (!textoNuevo.trim()) return;
+
+    const nueva ={
+      id:Date.now(),
+      texto:textoNuevo.trim(),
+      completada: false,
+      color: "grey",
+    };
+
+    const nuevas =[...tareas, nueva];
+    setTareas(nuevas);
+    guardarLs(nuevas);
+    setTextoNuevo("")
+  };
+  const toggleCompletada =(id) =>{
+    const nuevas = tareas.map ((t)=>
+    t.id === id ? {...t,completada : !t.completada} : t
+  );
+  setTareas (nuevas);
+  guardarLs (nuevas);
+  };
+  const eliminarTarea = (id) =>{
+    const nuevas = tareas.filter ((t) => t.id !== id);
+    setTareas(nuevas);
+    guardarLs(nuevas);
+  };
+  const cambiarColor = (id, color) => {
+    const nuevas = tareas.map ((t)=>
+    t.id === id ? {...t, color} : t
+  );
+  setBusqueda(nuevas);
+  guardarLs (nuevas);
+  };
+  const eliminarCompletadas = () =>{
+    const nuevas = tareas.filter ((t)=> !t.completada);
+    setTareas (nuevas);
+    guardarLs (nuevas);
+  };
+  const tareasFiltradas = tareas.filter ((t)=>
+   t.texto.toLowerCase().includes(busqueda.toLowerCase())
+ );
+ const tareasOrdenadas =[...tareasFiltradas].sort(
+  (a,b) => Number(a.completada) - Number (b.completada)
+ );
+  
   return (
     
     <div className="contenedor">
     <h1>To Do App React</h1>
       
-      <div className="Formulario">
+      <div className="formulario">
       <input
         type="text"
         placeholder="Nueva tarea"
-        value={textonuevo}
-        onchange={(e)=> setTextoNuevo(e.target.value)}
+        value={textoNuevo}
+        onChange={(e)=> setTextoNuevo(e.target.value)}
         className="input" 
         />
       <button className="boton" onClick={agregarTarea}>Agregar</button>
@@ -26,14 +83,14 @@ export default function AppR() {
       className="input"/>
       
       <button className="botonRojo"
-      onClick={eliminarCompletadas}>Elimninar completadas</button>
+      onClick={eliminarCompletadas}>Eliminar completadas</button>
 
       <div className="lista">
         {tareasOrdenadas.map((t) =>(
         <Notas
         key={t.id}
         tarea={t}
-        toggleCompletada={togglecompletada}
+        toggleCompletada={toggleCompletada}
         eliminarTarea={eliminarTarea}
         cambiarColor={cambiarColor}
         />
