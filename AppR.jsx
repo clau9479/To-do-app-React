cd545a864987bde8252866c75198f3a34a70c326
clau9479 <sienna_24@hotmail.com>
2025-12-01 02:17:38 -0300

    comienzo de mi proyecto

---
+++ AppR.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Notas from "./Notas.css";
+import Notas from "./Notas.jsx";
+
+export default function AppR() {
+  return (
+    <>
+    <h1>To Do App React</h1>
+    <div className="">
+      <Notas/>
+      <button className=""></button>
+    </div>
+    
+    
+    
+    </>
+  )
+}
+
+
